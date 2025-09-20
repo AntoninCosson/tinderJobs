d7ca24a83f66e6015b31dcc2d002fe0d3bca02b8
Cosson Antonin <cosson.antonin@gmail.com>
2025-09-21 01:14:06 +0200

    Delete connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,1 @@
-import mongoose from "mongoose";
 
-const mongoose = require('mongoose');
-
-const connectionString = process.env.CONNECTION_STRING; 
-
-mongoose.connect(connectionString, {
-    connectTimeoutMS: 2000,
-    dbName: 'BsReact',
-  })
- .then(() => {
-  console.log('Database connected')
-  console.log("Base actuelle :", mongoose.connection.name);
-})
- .catch(error => console.error(error));
